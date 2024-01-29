e4330fa111fdf7fca36e8f4b45b0121522ce510e
paul <paulentz@gmail.com>
2024-01-28 22:23:26 -0600

    cleaned up comment

--- Two_Sum_3.py
+++ Two_Sum_3.py
@@ -7,9 +7,9 @@ def twoSum(nums, target):
         remainder = target - num  #find the difference between the target and our current number
 
         #start evaluating nums to see if they're already in the list
-        
+
         if remainder in seen:               #evals if the difference between the target and the current number is already in the list, when it gets to 1, two will already be in the list, so it will return the index at 2 and the current index
-            return [seen[remainder], i]     #works because if the remainder is in there, a number that adds up to itself to equal the target is in the list, we need that numbers index and the current ones
+            return [seen[remainder], i]     #works because if the remainder is in there, a number that adds up to itself to equal the target is in the list, we need that numbers index and the current numbers index only
         
         else:
             seen[num] = i  #adds the current number and current index to the dictionary as a key value pair
