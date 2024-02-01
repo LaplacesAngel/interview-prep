c5991ca1cf25c4bc3d646a5776d0677d67be8596
paul <paulentz@gmail.com>
2024-02-01 09:59:57 -0600

    can solve with a nested loop but not a dict (yet)

--- good_pairs.py
+++ good_pairs.py
@@ -5,40 +5,48 @@
 # all about the indices here, if the index of a number is less than the index of another number in a list, 
 # and the numbers are the same, put the pair of the indices in an arrary
 
-def numIdenticalPairs(nums):
+class Solution:
+    def numIdenticalPairs(self, nums):
 
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
 
-    #number and index in list
-    dict = {
-        1:0,
-        2:1,
-        3:2,
-        1:3,
-        1:4,
-        3:5
-    }
+        #number and index in list
+        goodpairs = []
 
-    goodpairs = []
+        for i, num in enumerate(nums):
+            for j, numsagain in enumerate(nums):
+                if num == numsagain and i < j:
+                    pairs = (i, j)
+                    goodpairs.append(pairs)
 
+        return len(goodpairs)
     
 
-    for i in range(0, len(nums)):
-        for key, value in dict.items():
-            if nums[i] == key and i < value:
-                print(f"On round {i}: \n nums[i] is {nums[i]} \n key is {key} \n value is {value}")
-                tuple = (i, value)
-                print(tuple)
-                goodpairs.append(tuple)
+    #with a dict:
+    def numIdenticalPairsDict(self, nums):
 
-   
+        dict = {}
+        goodpairs = []
 
-    return len(goodpairs)
+        for i, num in enumerate(nums):
+            print(num, dict)
+            if num in dict and i < dict[num]:
+                print(num, dict[num])
+                goodpair = (i, dict[num])
+                goodpairs.append(goodpair)
+
+            dict[num] = i
+
+        return goodpairs
+
 
 
 nums = [1,2,3,1,1,3]
 
-print(numIdenticalPairs(nums))
+s1 = Solution()
+print(f"Nested loop solution: {s1.numIdenticalPairs(nums)}")
+print(f"Dictionary solution: {s1.numIdenticalPairsDict(nums)}")
+
